cf19cb92f37c3520dd9327e295ceac58211b5494
Michael <mlko53@stanford.edu>
2018-10-19 01:15:30 -0700

    clean and merge sid and mid cat scripts

--- scripts/tc_agg.R
+++ scripts/tc_agg.R
@@ -1,8 +1,15 @@
-# store all ROI timecourses in one csv with mid matrix for each subject jl161220
+########### Create  SID/MID matrix with ROI for each participant ##############
 scripts_dir = getwd()
 
 #### subject list ####
-subvec = c('test')
+subvec = c('dj051418', 'dy051818', 'gl052818', 'he042718', 'hw111117', 
+                'is060118', 'jd051818', 'mh071418', 'qh111717', 'rt022718', 
+                'sw050818', 'tl111017', 'wh071918', 'xl042618', 'xz071218', 
+                'yd081018', 'yg042518', 'yl070418', 'yl070518', 'yl080118', 
+                'yp070418', 'yq052218', 'yw070618', 'yw081018', 'yx072518')
+
+#### epi list ####
+epi = c('sid', 'mid')
 
 #### create dataframe to store values ####
 d.f=data.frame(
@@ -18,67 +25,89 @@ d.f=data.frame(
   l_acing_raw.tc=character(),l_caudate_raw.tc=character(),l_vlpfc_raw.tc=character(),
   stringsAsFactors=F)
 
-#### for each subject ####
-for (i in 1:length(subvec)){
-  # construct filenames to load data
-  matrix_filename = paste0(dirname(scripts_dir), '/data/bhvr/', subvec[i],'_sid_matrix.csv')
-  write_fullskewmat_filename = paste0(dirname(scripts_dir), '/data/sid_tcs/', subvec[i],'_sidtc8tr.csv')
-  
-  filenames = c()
-  for (roi in 1:length(colnames(d.f))){
-    filenames[roi] = paste0(dirname(scripts_dir), '/data/fmri/', subvec[i], '/sid_tcs/', subvec[i],"_",colnames(d.f)[roi])
-  }
-  
-  # load sid matrix into dataframe
-  d.sidmat = read.csv(matrix_filename, head=T)
+for(task in epi){
+  #### for each subject ####
+  for (i in 1:length(subvec)){
+    # construct filenames to load data
+    matrix_filename = paste0(dirname(scripts_dir), '/data/bhvr/', subvec[i], '_', task,'_matrix.csv')
+    write_fullskewmat_filename = paste0(dirname(scripts_dir), '/output/', task, '_tcs/', subvec[i],'_', task, 'tc8tr.csv')
+    
+    filenames = c()
+    for (roi in 1:length(colnames(d.f))){
+      filenames[roi] = paste0(dirname(scripts_dir), '/data/fmri/', subvec[i], '/', task, '_tcs/', subvec[i],"_",colnames(d.f)[roi])
+    }
+    
+    # load matrix into dataframe
+    d.mat = read.csv(matrix_filename, head=T)
 
-  # load time courses
-  dataframenames = c()
-  for (roi in 1:length(colnames(d.f))){
-    dataframenames[roi] = paste('d.',colnames(d.f)[roi], sep='')
+    # load time courses
+    dataframenames = c()
+    for (roi in 1:length(colnames(d.f))){
+      dataframenames[roi] = paste('d.',colnames(d.f)[roi], sep='')
+    }
+    
+    # append time courses into data frame
+    timecourses = data.frame(matrix(nrow=384,ncol=length(colnames(d.f))))
+    timecourses[,1] = rep(subvec[i],384)
+    for (tc in 2:length(colnames(d.f))){
+      timecourses[,tc] = read.table(filenames[tc])
+    }
+    colnames(timecourses) = colnames(d.f) # get ROI for colnames
+    
+    # 4 TR's of interest per trial (*2 seconds per TR), so we want 8 TR's per trial, 64*8=512
+    tc512 = cbind(d.mat, timecourses)
+    # make each trial 8 TRs long, with proper trial number, condition, etc.
+    tc8tr = c()
+    trialonset = which(d.mat$TR==1)
+    for(start in trialonset){
+      end=start+7
+      TR=c(1:8)
+      trial.tmp = data.frame(rep(tc512[start,'trial'], 8),
+                             TR,
+                             rep(tc512[start, 'ethni_r'],8),
+                             rep(tc512[start, 'trialonset'],8),
+                             rep(tc512[start, 'trialtype'],8),
+                             rep(tc512[start, 'target_ms'],8),
+                             rep(tc512[start, 'rt'],8),
+                             rep(tc512[start, 'cue_value'],8),
+                             rep(tc512[start, 'hit'],8),
+                             rep(tc512[start, 'iti'],8),
+                             rep(tc512[start, 'drift'],8),
+                             rep(tc512[start, 'total_winpercent'],8),
+                             rep(tc512[start, 'binned_winpercent'],8))
+      if(task == 'sid'){
+        trial.tmp.2 = data.frame(rep(tc512[start, 'valence'],8),
+                             rep(tc512[start, 'level'],8),
+                             rep(tc512[start, 'ethni_t'],8),
+                             rep(tc512[start, 'gender_t'],8),
+                             tc512[start:end, colnames(d.f)])
+      } else if(task == 'mid'){
+        trial.tmp.2 = data.frame(tc512[start:end, colnames(d.f)])
+      }
+      tc8tr = rbind(tc8tr, cbind(trial.tmp, trial.tmp.2))
+    }
+    
+    if (task == 'sid'){
+      colnames(tc8tr)[1:17] = colnames(tc512[1:17])
+    }
+    else if(task == 'mid'){
+      colnames(tc8tr)[1:13] = colnames(tc512[1:13])
+    }
+    trialnum = c(1:64)
+    x = tc8tr
+    # exclude last row with NA's
+    d.tc8tr = as.data.frame(tc8tr[1:511,])
+    write.csv(d.tc8tr, write_fullskewmat_filename, row.names=F)
   }
-  
-  # append time courses into data frame
-  timecourses = data.frame(matrix(nrow=384,ncol=length(colnames(d.f))))
-  timecourses[,1] = rep(subvec[i],384)
-  for (tc in 2:length(colnames(d.f))){
-    timecourses[,tc] = read.table(filenames[tc])
-  }
-  colnames(timecourses) = colnames(d.f) # get ROI for colnames
-  
-  # 4 TR's of interest per trial (*2 seconds per TR), so we want 8 TR's per trial, 64*8=512
-  d.sidtc512 = cbind(d.sidmat, timecourses)
+}
 
-  # make each trial 8 TRs long, with proper trial number, condition, etc.
-  sidtc8tr = c()
-  trialonset = which(d.sidmat$TR==1)
-  for(start in trialonset){
-    end=start+7
-    TR=c(1,2,3,4,5,6,7,8)
-    trial.tmp = data.frame(rep(d.sidtc512[start,1],8),# trial
-                           TR,
-                           rep(d.sidtc512[start,3],8),# ethni_r
-                           rep(d.sidtc512[start,4],8),# trialonset
-                           rep(d.sidtc512[start,5],8),# trialtype
-                           rep(d.sidtc512[start,6],8),# target_ms
-                           rep(d.sidtc512[start,7],8),# rt
-                           rep(d.sidtc512[start,8],8),# cue_value
-                           rep(d.sidtc512[start,9],8),# hit
-                           rep(d.sidtc512[start,10],8),# iti
-                           rep(d.sidtc512[start,11],8),# drift
-                           rep(d.sidtc512[start,12],8),# total_winpercent
-                           rep(d.sidtc512[start,13],8),# binned_winpercent
-                           rep(d.sidtc512[start,14],8),# valence
-                           rep(d.sidtc512[start,15],8),# level
-                           rep(d.sidtc512[start,16],8),# ethni_t
-                           rep(d.sidtc512[start,17],8),# gender_t
-                           d.sidtc512[start:end,18:45])
-    sidtc8tr = rbind(sidtc8tr,trial.tmp)
+###### Concat all the subjects together ################
+for(task in epi){
+  d = c()
+  for(sub in subvec){
+    loadfilename = paste0('../output/',task, '_tcs/', sub, '_', task, 'tc8tr.csv')
+    temp_d = read.csv(loadfilename, head=T)
+    d = rbind(d, temp_d)
   }
-  colnames(sidtc8tr)[1:17] = colnames(d.sidtc512[1:17])
-  trialnum = c(1:64)
-  
-  # exclude last row with NA's
-  d.sidtc8tr = as.data.frame(sidtc8tr[1:511,])
-  write.csv(d.sidtc8tr, write_fullskewmat_filename, row.names=F)
+  write.csv(d, paste0('../output/', task, '_tcs/', task, '_timecourse.csv'), row.names=F)
 }
