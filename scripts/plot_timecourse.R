8c611bd09d06bee6574f999182cdefcbb37d36e8
Michael <mlko53@stanford.edu>
2019-02-04 01:21:25 -0800

    reduce redundancy

--- scripts/plot_timecourse.R
+++ scripts/plot_timecourse.R
@@ -1,6 +1,5 @@
 # plot time courses by TR and MID condition jl160202
 # updated by eb for SID 062018
-
 require("ggplot2")
 require("ggthemes")
 require("reshape2")
@@ -37,9 +36,26 @@ sid.tc <- sid.tc[c('trial','TR','trialtype','subject','ethni_r','hit',
                        'l_vlpfc_raw.tc','r_vlpfc_raw.tc',
                        'l_caudate_raw.tc','r_caudate_raw.tc')]
 
+# format data columns
+sid.tc = sid.tc %>%
+  # convert ethnicity to factor and recode 
+  mutate(ethni_r = factor(ethni_r, levels = c("0","1"))) %>%
+  mutate(ethni_r = fct_recode(ethni_r, "EA"= "0", "CH" = "1")) %>%
+  # convert trialtype to factor and recode
+  mutate(trialtype = factor(trialtype, levels = c("1","2","3","4","5","6","7","8"))) %>%
+  mutate(trialtype = fct_recode(trialtype,
+                                  "lan"= "1", "man" = "2", "han" = "3",
+                                  "lap"= "4", "map" = "5", "hap" = "6",
+                                  "neun" = "7", "neup" = "8"))
+
+# convert to factors
+sid.tc$hit = as.factor(sid.tc$hit)
+sid.tc$hit = fct_recode(sid.tc$hit, "miss" = "0", "hit" = "1")
+
 ####### Tidy Data #######
 #########################
 
+# convert to long form
 sid.tc.long.sum.bin = sid.tc %>%
   gather(variable, value, l_nacc8mm_raw.tc:r_caudate_raw.tc) %>%
   group_by(TR, trialtype, variable, ethni_r, subject) %>%
@@ -47,23 +63,13 @@ sid.tc.long.sum.bin = sid.tc %>%
             se.subject = sd(value)/sqrt(length(value))) %>%
   summarise(mean = mean(mean.subject),
             se.mean = sd(mean.subject)/sqrt(length(unique(sid.tc$subject)))) %>%
-  # convert ethnicity to factor and recode
-  mutate(ethni_r = factor(ethni_r, levels = c("0","1"))) %>%
-  mutate(ethni_r = fct_recode(ethni_r, "EA"= "0", "CH" = "1")) %>%
-  # convert trialtype to factor and recode
-  mutate(trialtype_f = factor(trialtype, levels = c("1","2","3","4","5","6","7","8"))) %>%
-  mutate(trialtype_f = fct_recode(trialtype_f,
-                                  "lan"= "1", "man" = "2", "han" = "3",
-                                  "lap"= "4", "map" = "5", "hap" = "6",
-                                  "neun" = "7", "neup" = "8")) %>%
   # add column for valence
-  mutate(vale = case_when(grepl("n$", trialtype_f) ~ 'neg',
-                          grepl("p$", trialtype_f) ~ 'pos')) %>%
+  mutate(vale = case_when(grepl("n$", trialtype) ~ 'neg',
+                          grepl("p$", trialtype) ~ 'pos')) %>%
   # add column for hemisphere
   mutate(hemi = case_when(grepl("l_", variable) ~ 'left',
                           grepl("r_", variable) ~ 'right'))
 
-# convert to long form
 sid.tc.long.sum = sid.tc %>%
   gather(variable, value, l_nacc8mm_raw.tc:r_caudate_raw.tc) %>%
   group_by(TR, trialtype, variable, hit, ethni_r, subject) %>%
@@ -71,36 +77,22 @@ sid.tc.long.sum = sid.tc %>%
             se.subject = sd(value)/sqrt(length(value))) %>%
   summarise(mean = mean(mean.subject),
             se.mean = sd(mean.subject)/sqrt(length(unique(sid.tc$subject)))) %>%
-  # convert ethnicity to factor and recode
-  mutate(ethni_r = factor(ethni_r, levels = c("0","1"))) %>%
-  mutate(ethni_r = fct_recode(ethni_r, "EA"= "0", "CH" = "1")) %>%
-  # convert trialtype to factor and recode
-  mutate(trialtype_f = factor(trialtype, levels = c("1","2","3","4","5","6","7","8"))) %>%
-  mutate(trialtype_f = fct_recode(trialtype_f,
-                                  "lan"= "1", "man" = "2", "han" = "3",
-                                  "lap"= "4", "map" = "5", "hap" = "6",
-                                  "neun" = "7", "neup" = "8")) %>%
   # add column for valence
-  mutate(vale = case_when(grepl("n$", trialtype_f) ~ 'neg',
-                          grepl("p$", trialtype_f) ~ 'pos')) %>%
+  mutate(vale = case_when(grepl("n$", trialtype) ~ 'neg',
+                            grepl("p$", trialtype) ~ 'pos')) %>%
   # add column for hemisphere
   mutate(hemi = case_when(grepl("l_", variable) ~ 'left',
                           grepl("r_", variable) ~ 'right'))
 
-# convert to factors
 sid.tc.long.sum.bin$vale = as.factor(sid.tc.long.sum.bin$vale)
 sid.tc.long.sum.bin$variable = as.factor(sid.tc.long.sum.bin$variable)
-sid.tc.long.sum.bin$variable = substr(sid.tc.long.sum.bin$variable, start = 3, stop = str_length(sid.tc.long.sum.bin$variable))
+sid.tc.long.sum.bin$variable = substr(sid.tc.long.sum.bin$variable, start = 3, stop = 999)
 
 sid.tc.long.sum$vale = as.factor(sid.tc.long.sum$vale)
 sid.tc.long.sum$variable = as.factor(sid.tc.long.sum$variable)
-sid.tc.long.sum$variable = substr(sid.tc.long.sum$variable, start = 3, stop = str_length(sid.tc.long.sum$variable))
-sid.tc.long.sum$hit = as.factor(sid.tc.long.sum$hit)
-sid.tc.long.sum$hit = fct_recode(sid.tc.long.sum$hit,
-                               "miss" = "0", "hit" = "1")
+sid.tc.long.sum$variable = substr(sid.tc.long.sum$variable, start = 3, stop = 999) 
 
 # Created inverse weighted variance dataframes
-
 sid.tc.long.sum.bin.inv = sid.tc %>%
   gather(variable, value, l_nacc8mm_raw.tc:r_caudate_raw.tc) %>%
   group_by(TR, trialtype, variable, ethni_r, subject) %>%
@@ -108,18 +100,9 @@ sid.tc.long.sum.bin.inv = sid.tc %>%
             var.subject = var(value)) %>%
   summarise(mean = wtd.mean(mean.subject, 1/var.subject),
             se.mean = sqrt(wtd.var(mean.subject, 1/var.subject))/sqrt(length(unique(sid.tc$subject)))) %>%
-  # convert ethnicity to factor and recode
-  mutate(ethni_r = factor(ethni_r, levels = c("0","1"))) %>%
-  mutate(ethni_r = fct_recode(ethni_r, "EA"= "0", "CH" = "1")) %>%
-  # convert trialtype to factor and recode
-  mutate(trialtype_f = factor(trialtype, levels = c("1","2","3","4","5","6","7","8"))) %>%
-  mutate(trialtype_f = fct_recode(trialtype_f,
-                                  "lan"= "1", "man" = "2", "han" = "3",
-                                  "lap"= "4", "map" = "5", "hap" = "6",
-                                  "neun" = "7", "neup" = "8")) %>%
   # add column for valence
-  mutate(vale = case_when(grepl("n$", trialtype_f) ~ 'neg',
-                          grepl("p$", trialtype_f) ~ 'pos')) %>%
+  mutate(vale = case_when(grepl("n$", trialtype) ~ 'neg',
+                            grepl("p$", trialtype) ~ 'pos')) %>%
   # add column for hemisphere
   mutate(hemi = case_when(grepl("l_", variable) ~ 'left',
                           grepl("r_", variable) ~ 'right'))
@@ -131,18 +114,9 @@ sid.tc.long.sum.inv = sid.tc %>%
             var.subject = var(value)) %>%
   summarise(mean = wtd.mean(mean.subject, 1/var.subject),
             se.mean = sqrt(wtd.var(mean.subject, 1/var.subject))/sqrt(length(unique(sid.tc$subject)))) %>%
-  # convert ethnicity to factor and recode
-  mutate(ethni_r = factor(ethni_r, levels = c("0","1"))) %>%
-  mutate(ethni_r = fct_recode(ethni_r, "EA"= "0", "CH" = "1")) %>%
-  # convert trialtype to factor and recode
-  mutate(trialtype_f = factor(trialtype, levels = c("1","2","3","4","5","6","7","8"))) %>%
-  mutate(trialtype_f = fct_recode(trialtype_f,
-                                  "lan"= "1", "man" = "2", "han" = "3",
-                                  "lap"= "4", "map" = "5", "hap" = "6",
-                                  "neun" = "7", "neup" = "8")) %>%
   # add column for valence
-  mutate(vale = case_when(grepl("n$", trialtype_f) ~ 'neg',
-                          grepl("p$", trialtype_f) ~ 'pos')) %>%
+  mutate(vale = case_when(grepl("n$", trialtype) ~ 'neg',
+                            grepl("p$", trialtype) ~ 'pos')) %>%
   # add column for hemisphere
   mutate(hemi = case_when(grepl("l_", variable) ~ 'left',
                           grepl("r_", variable) ~ 'right'))
@@ -150,14 +124,11 @@ sid.tc.long.sum.inv = sid.tc %>%
 # convert to factors
 sid.tc.long.sum.bin.inv$vale = as.factor(sid.tc.long.sum.bin.inv$vale)
 sid.tc.long.sum.bin.inv$variable = as.factor(sid.tc.long.sum.bin.inv$variable)
-sid.tc.long.sum.bin.inv$variable = substr(sid.tc.long.sum.bin.inv$variable, start = 3, stop = str_length(sid.tc.long.sum.bin.inv$variable))
+sid.tc.long.sum.bin.inv$variable = substr(sid.tc.long.sum.bin.inv$variable, start = 3, stop = 999)
 
 sid.tc.long.sum.inv$vale = as.factor(sid.tc.long.sum.inv$vale)
 sid.tc.long.sum.inv$variable = as.factor(sid.tc.long.sum.inv$variable)
-sid.tc.long.sum.inv$variable = substr(sid.tc.long.sum.inv$variable, start = 3, stop = str_length(sid.tc.long.sum.inv$variable))
-sid.tc.long.sum.inv$hit = as.factor(sid.tc.long.sum.inv$hit)
-sid.tc.long.sum.inv$hit = fct_recode(sid.tc.long.sum.inv$hit,
-                               "miss" = "0", "hit" = "1")
+sid.tc.long.sum.inv$variable = substr(sid.tc.long.sum.inv$variable, start = 3, stop = 999)
 
 ## Read and Precompute MID csv
 
@@ -348,12 +319,12 @@ savePlot = function(p, name, path){
 plotBin = function(d, area, val){
   d1 = d %>%
     filter(variable == area & vale == val)
-  d1$trialtype_f = factor(d1$trialtype_f)
+  d1$trialtype = factor(d1$trialtype)
   tcplot = ggplot(
-    d1, aes(x = TR, y = mean, group = trialtype_f, shape = trialtype_f)) + 
+    d1, aes(x = TR, y = mean, group = trialtype, shape = trialtype)) + 
     geom_errorbar(aes(ymin = mean-se.mean, ymax = mean+se.mean), width = 0.25) +
-    geom_line(aes(linetype = trialtype_f, colour = trialtype_f), size = 1) +
-    geom_point(aes(shape = trialtype_f, colour = trialtype_f, fill = trialtype_f), size = 3.5, shape = 21) +
+    geom_line(aes(linetype = trialtype, colour = trialtype), size = 1) +
+    geom_point(aes(shape = trialtype, colour = trialtype, fill = trialtype), size = 3.5, shape = 21) +
     scale_fill_manual(values = c("#91bfdb","#ff9326","#e41a1c","#bdbdbd")) +
     scale_color_manual(values = c("#91bfdb","#ff9326","#e41a1c","#bdbdbd")) +
     scale_x_continuous(breaks = seq(0,8, by = 1), labels = seq(0,16, by = 2)) +
@@ -373,10 +344,10 @@ plotInd = function(d, area, val, outcome){
   d1 = d %>%
     filter(variable == area & hit == outcome & vale == val)
   tcplot = ggplot(
-    d1, aes(x = TR, y = mean, group = trialtype_f, shape = trialtype_f)) + 
+    d1, aes(x = TR, y = mean, group = trialtype, shape = trialtype)) + 
     geom_errorbar(aes(ymin = mean-se.mean, ymax = mean+se.mean), width = 0.25) +
-    geom_line(aes(linetype = trialtype_f, colour = trialtype_f), size = 1) +
-    geom_point(aes(shape = trialtype_f, colour = trialtype_f, fill = trialtype_f), size = 3.5, shape = 21) +
+    geom_line(aes(linetype = trialtype, colour = trialtype), size = 1) +
+    geom_point(aes(shape = trialtype, colour = trialtype, fill = trialtype), size = 3.5, shape = 21) +
     scale_fill_manual(values = c("#91bfdb","#ff9326","#e41a1c","#bdbdbd")) +
     scale_color_manual(values = c("#91bfdb","#ff9326","#e41a1c","#bdbdbd")) +
     scale_x_continuous(breaks = seq(0,8, by = 1), labels = seq(0,16, by = 2)) +
@@ -396,10 +367,10 @@ plotHVM = function(d, area, val){
   d1 <- d %>%
     filter(variable==area & vale==val)
   tcplot = 
-    ggplot(d1, aes(x = TR, y = mean, shape = hit, color = trialtype_f)) +
+    ggplot(d1, aes(x = TR, y = mean, shape = hit, color = trialtype)) +
     geom_errorbar(aes(ymin = mean-se.mean, ymax = mean+se.mean), width = 0.25) +
-    geom_point(aes(group = interaction(trialtype_f, hit)), size = 3.5) +
-    geom_line(aes(color = trialtype_f, linetype = hit), size = 1) +
+    geom_point(aes(group = interaction(trialtype, hit)), size = 3.5) +
+    geom_line(aes(color = trialtype, linetype = hit), size = 1) +
     scale_linetype_manual(values=c("dashed", "solid")) +  
     scale_shape_manual(values = c(1, 16)) +
     scale_fill_manual(values = c("#91bfdb","#ff9326","#e41a1c","#bdbdbd")) +
@@ -417,6 +388,40 @@ plotHVM = function(d, area, val){
   return(tcplot)
 }
 
+####### Plot Bar ########
+#########################
+
+plotBar = function(task, area, side, val, outcome, TR){
+    if(task=="sid"){
+        df = sid.tc.long.sum
+    } else{
+        if(task=="mid"){
+            df = mid.tc.long.sum
+        }
+    }
+    df = df %>%
+        filter(variable == area & vale == val & TR == TR)
+    # select area
+    if(side=="both"){
+        df = df %>% 
+            group_by(ethni_r, trialtype_f, hit) %>%
+            summarise(mean = mean(mean))
+    } else{
+        df = df %>%
+            filter(hemi==side)
+    }
+    # select outcome
+    if(outcome=="area"){
+        df = df %>%
+            group_by(ethni_r, trialtype_f) %>%
+            summarise(mean = mean(mean))
+    } else{
+        df = df %>%
+            filter(hit==outcome)
+    }
+    return(df)
+}
+
 ####### Plot Data #######
 #########################
 # all areas have pos vs neg, bin vs hit vs miss vs (hit v miss) #
@@ -427,7 +432,7 @@ plotHVM = function(d, area, val){
 # nacc.gain.tcplot.hvm
 # nacc.gain.tcplot
 # nacc.loss.tcplot
-p1 <- plotBin(sid.tc.long.sum.bin, 'nacc8mm_raw.tc', 'pos')
-p2 <- plotInd(sid.tc.long.sum, 'nacc8mm_raw.tc', 'pos', 'hit')
+#p1 <- plotBin(sid.tc.long.sum.bin, 'nacc8mm_raw.tc', 'pos')
+#p2 <- plotInd(sid.tc.long.sum, 'nacc8mm_raw.tc', 'pos', 'hit')
 
 # savePlot(p1, 'test.pdf', '../output/figures/')
