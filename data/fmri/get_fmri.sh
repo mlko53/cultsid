7477cb99f9a91b78d21c01bc3abeb5c016e64040
Michael <mlko53@stanford.edu>
2020-08-19 19:28:48 -0700

    add uncommited changes

--- data/fmri/get_fmri.sh
+++ data/fmri/get_fmri.sh
@@ -1,7 +1,7 @@
 echo -e "alias fw='$HOME/flywheel/cli/fw'" >> $HOME/.bashrc
 source $HOME/.bashrc
 fw download "knutson/cultsid" -i nifti -i bvec -i bval
-tar -tvf cultsid.tar
+tar -xvf cultsid.tar
 rm cultsid.tar
 mv scitran/knutson/cultsid/* .
 rm -rf scitran
